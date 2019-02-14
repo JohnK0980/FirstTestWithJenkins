0a404fc840a33c07e5523a420ac1becfbdbce896
John <kkj0980@gmail.com>
2019-02-14 16:19:05 +1300

    Adding test case
    Navigate to Google website

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class LoginTest {
     public WebDriver driver = new ChromeDriver();
 
-
+    @Test
+    public void goToGoogle() {
+        driver.get("http://google.com");
+    }
 }
